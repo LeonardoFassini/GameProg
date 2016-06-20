52d865989f25980fe5432b336106437dd1be53b7
ninakolovu <leehtironi@gmail.com>
2016-06-20 00:40:02 -0300

    Added dino and collision with him

--- Documents/NetBeansProjects/ZeldaBugado/src/zeldabugado/Boneco.java
+++ Documents/NetBeansProjects/ZeldaBugado/src/zeldabugado/Boneco.java
@@ -1,14 +1,19 @@
 package zeldabugado;
 
+import jplay.Collision;
 import jplay.Keyboard;
 import jplay.Sprite;
 import jplay.Window;
 
-public class Boneco extends Sprite {
+public class Boneco extends Sprite
+{
+
     private int sentido;
     private int sAnterior;
-    boolean flag = true;
-    int count = 0;
+    boolean flag;
+    int count;
+    boolean alive;
+
     public Boneco(int x, int y)
     {
         super("bnc.png", 8);
@@ -18,38 +23,37 @@ public class Boneco extends Sprite {
         setCurrFrame(1);
         this.x = x;
         this.y = y;
-
+        flag = true;
+        count = 0;
+        alive = true;
     }
-    
+
     public void move(Window tela, Keyboard teclado)
     {
         // Verificar se a seta pra esquerda esta pressionada
-        if(teclado.keyDown(Keyboard.LEFT_KEY) && this.x >= 10) 
+        if (teclado.keyDown(Keyboard.LEFT_KEY) && this.x >= 10)
         {
-            if(sentido != Constantes.LEFT)
+            if (sentido != Constantes.LEFT)
             {
-                setSequence(1,4);
+                setSequence(1, 4);
                 sAnterior = Constantes.LEFT;
                 sentido = Constantes.LEFT;
             }
-            
+
             this.x -= 0.5;
-        }
-        else
+        } else
         {
             // Verificar se a seta pra direita está pressionada
-            if(teclado.keyDown(Keyboard.RIGHT_KEY) && ((this.x + this.width) < tela.getWidth()))
+            if (teclado.keyDown(Keyboard.RIGHT_KEY) && ((this.x + this.width) < tela.getWidth()))
             {
                 if (sentido != Constantes.RIGHT)
                 {
-                    setSequence(5,8);
+                    setSequence(5, 8);
                     sAnterior = Constantes.RIGHT;
                     sentido = Constantes.RIGHT;
                 }
                 this.x += 0.5; //VERIFICAR ISSO;
-            }
-            
-            else
+            } else
             {
                 // Se nenhuma das duas esta pressionada, entao ele esta parado
                 sentido = Constantes.STOP;
@@ -64,19 +68,19 @@ public class Boneco extends Sprite {
         }
         // Verifico se eu pressionei o botao de pula
         if (teclado.keyDown(Keyboard.UP_KEY))
-            {
-                flag = false;
-            }
-        
+        {
+            flag = false;
+        }
+
         // Se o botao de pular foi pressionado uma vez, entao ele faz a animacao
-        if(flag == false)
+        if (flag == false)
         {
             if (count <= 149)
             {
                 this.y -= 0.5;
                 count++;
             }
-            if (count >= 150 && count <= 300)
+            if (count >= 150 && count <= 299)
             {
                 this.y += 0.5;
                 count++;
@@ -93,5 +97,39 @@ public class Boneco extends Sprite {
             update();
         }
     }
+
+    public int busted(Dino dino)
+    {
+        if (dino.getAlive())
+        {
+            if ((dino.x > this.x && dino.x < this.x + this.width) || (dino.x + dino.width > this.x && dino.x + dino.width < this.x + this.width))
+            {
+                if (this.y + this.height == dino.y)
+                {
+                    return 1;
+
+                } else
+                {
+                    if (this.y + this.height > dino.y)
+                    {
+                        if ((this.x > dino.x && this.x < dino.x + dino.width) || (this.x + this.width > dino.x && this.x + this.width < dino.x + dino.width))
+                        {
+                            return 2;
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    public void matarBoneco()
+    {
+        this.alive = false;
+    }
+
+    public boolean getAlive()
+    {
+        return this.alive;
+    }
 }
-    
--- Documents/NetBeansProjects/ZeldaBugado/src/zeldabugado/ZeldaBugado.java
+++ Documents/NetBeansProjects/ZeldaBugado/src/zeldabugado/ZeldaBugado.java
@@ -15,10 +15,6 @@ public class ZeldaBugado
     Window window;
     Mouse mouse;
     Keyboard keyboard;
-    BackGround backGround;
-    GameImage buttonStart;
-    GameImage buttonExit;
-    Boneco boneco;
 
     /**
      *
@@ -38,60 +34,10 @@ public class ZeldaBugado
         keyboard = window.getKeyboard(); // Inicializar o Keyboard
         keyboard.addKey(KeyEvent.VK_Z, Keyboard.DETECT_INITIAL_PRESS_ONLY); // Adicionar a tecla Z
         mouse = window.getMouse(); //Inicializar o Mouse
-        backGround = new BackGround(); // BackGround
-        buttonStart = new GameImage("buttonStart.png");
-        buttonExit = new GameImage("buttonExit.png");
-        //Coordenadas dos botoes do menu001;
-        //buttons
-        buttonStart.y = (window.getHeight() / 4); // Botão de iniciar
-        buttonStart.x = 250.0;
-        buttonStart.height = 50;
-        buttonStart.width = 341;
-        buttonExit.y = (3 * (window.getHeight() / 4)); // Botão de sair
-        buttonExit.x = 250.0;
-        buttonExit.height = 50;
-        buttonExit.width = 341;
     }
 
     private void loop()
     {
-        boolean executing = true;
-        int floor = 550;
-        while (executing)
-        {
-            backGround.setBackGround("bgInicio.png");
-            backGround.draw();
-            buttonStart.draw();
-            buttonExit.draw();
-            window.update();
-            if (mouse.isLeftButtonPressed() == true)
-            {
-                Point posicao = mouse.getPosition();
-                if (mouse.isOverObject(buttonExit))
-                {
-                    executing = false;
-                }
-                if (mouse.isOverObject(buttonStart))
-                {
-                    backGround.setBackGround("bg1.png");
-                    boneco = new Boneco(10, 500);
-                    boolean gameGoing = true;
-                    boolean flag = true;
-                    while (gameGoing == true)
-                    {
-
-                        backGround.draw();
-                        boneco.draw();
-                        window.update();
-                        boneco.move(window, keyboard);
-                        if (keyboard.keyDown(Keyboard.ESCAPE_KEY))    
-                        {
-                            gameGoing = false;
-                        }
-
-                    }
-                }
-            }
-        }
+        new Intro(window, keyboard, mouse);
     }
 }
